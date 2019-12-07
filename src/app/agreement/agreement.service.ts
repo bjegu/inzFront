6658b9d253f98cc7e96d0f44986ca1801710bc1a
Dominika Bieg <dominikabieg@gmail.com>
2019-12-07 15:33:11 +0100

    fix agrement prod api

--- src/app/agreement/agreement.service.ts
+++ src/app/agreement/agreement.service.ts
@@ -2,20 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Agreement } from './agreement.model';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgreementService {
 
+  apiGeneral = environment.apiUrl;
+
   constructor(private httpClient: HttpClient) { }
 
   getAgreement():Observable<Agreement[]>{
-    return this.httpClient.get<Agreement[]>('http://localhost:4200/api/agreement/')
+    return this.httpClient.get<Agreement[]>(this.apiGeneral+'agreement/')
   }
 
   deleteAgreement():Observable<Agreement>{
-    return this.httpClient.delete<Agreement>('http://localhost:4200/api/agreement/delete/{id}')
+    return this.httpClient.delete<Agreement>(this.apiGeneral+ 'agreement/delete/{id}')
   }
 
 }
