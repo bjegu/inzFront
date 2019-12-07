78635f7b9f7e5dfdb7147baed6992ef32e2dd888
Dominika Bieg <dominikabieg@gmail.com>
2019-12-07 15:25:24 +0100

    fix cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+var cors = require('cors')
 const app = express();
 
 // Serve static files....
@@ -9,6 +10,8 @@ app.use(express.static(__dirname + '/dist/inz2'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/inz2/index.html'));
 });
+
+app.options('*', cors())
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
