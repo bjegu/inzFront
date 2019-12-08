15ffe544c98d9581e112834988214839586d5c08
Dominika Bieg <dominikabieg@gmail.com>
2019-12-08 20:03:14 +0100

    whitelist cors api

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const path = require('path');
 var cors = require('cors')
 const app = express();
 
+var corsOptions = {
+  origin: 'https://client-backend.herokuapp.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 // Serve static files....
 app.use(express.static(__dirname + '/dist/inz2'));
 
@@ -11,7 +16,7 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/inz2/index.html'));
 });
 
-app.options('*', cors())
+app.options('*', cors(corsOptions))
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
