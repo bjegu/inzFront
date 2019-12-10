3f533d8438332a5c6660c9230584b99a6bbf0aa6
Dominika Bieg <dominikabieg@gmail.com>
2019-12-10 20:59:08 +0100

    heroku proxy middleware

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const path = require('path');
-var cors = require('cors')
 const app = express();
-
-var corsOptions = {
-  origin: 'https://client-backend.herokuapp.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+var proxy = require('http-proxy-middleware');
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/inz2'));
+
+app.use(
+    '/api',
+    proxy({ target: 'https://client-backend.herokuapp.com', changeOrigin: true })
+  );
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/inz2/index.html'));
 });
-
-app.options('*', cors(corsOptions))
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
