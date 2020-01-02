dad2c215a2a1fe4fd9be94d79078921bc7f99f61
Dominika Bieg <dominikabieg@gmail.com>
2020-01-02 13:37:07 +0100

    add calendar form

--- src/app/calendar/event.model.ts
+++ src/app/calendar/event.model.ts
@@ -1,13 +1,21 @@
+import { Client } from '../client/client.model';
+
 export interface Event {
     uuid: string;
     start: string;
     finish: string;
     name: string;
-    description?:string;
+    description?: string;
     eventType: EventType;
 }
 
-export interface EventType{
+export interface EventType {
     id: number;
     eventTypeName: string;
+}
+
+export interface EventClients {
+    name: string;
+    phone: string;
+    client?: Client;
 }
