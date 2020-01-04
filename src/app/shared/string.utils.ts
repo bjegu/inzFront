a74fa7645302889b6c1953c20a3c47aa875e8218
Dominika Bieg <dominikabieg@gmail.com>
2020-01-04 17:30:03 +0100

    add edition of event

--- src/app/shared/string.utils.ts
+++ src/app/shared/string.utils.ts
@@ -1,4 +1,5 @@
 export class StringUtils {
+  
     public static convertDate(date: { year: number, month: number, day: number }): string {
         return date.year + '-' + this.leadingZero(date.month) + '-' + this.leadingZero(date.day)
     }
@@ -20,5 +21,12 @@ export class StringUtils {
         return this.leadingZero(date.getHours()) + ':' + this.leadingZero( date.getMinutes())
     }
 
+    public static convertDateToNbDate(date:Date): { year: number, month: number, day: number }{
+            return { year: date.getFullYear(), month: date.getMonth()+1, day: date.getUTCDate() }
+    }
+    public static convertDateToNbTime(date:Date): {hour: number, minute: number, second: number}{
+        return {hour: date.getHours(), minute: date.getMinutes(), second: date.getSeconds()}
+}
+    
 
 }
