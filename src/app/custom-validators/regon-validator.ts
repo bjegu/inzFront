dfcef859f47cd87470d773e748a838654ca9ad61
Dominika Bieg <dominikabieg@gmail.com>
2020-01-04 19:57:35 +0100

    fix regon validation bug

--- src/app/custom-validators/regon-validator.ts
+++ src/app/custom-validators/regon-validator.ts
@@ -2,10 +2,13 @@ import { FormControl } from '@angular/forms';
 
 export function lengthValidator(control: FormControl){
     let regon = control.value;
-    if(regon != null && (regon.length !==9 && regon.length!== 14)){
-        return {
-            regonLength: regon || "cos"
-        }
+    if(regon != null){
+        let value = String(regon)
+        if(value.length !==9 && value.length!== 14){
+            return {
+                regonLength: regon || "cos"
+            }
+        } 
     }
     return null;
 }
